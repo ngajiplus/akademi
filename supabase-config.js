94794157699fb2d240a7d186e4e8c96a6c7cfbdd
ngajiplus <al.ngajiplus@gmail.com>
2026-08-30 16:06:18 +0700

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -5,7 +5,7 @@ const SUPABASE_ANON_KEY =
   "sb_publishable_HmFKwHMCISfN_YOoiLx3-A_MWnG9yG7";
 
 
-const supabaseClient = supabase.createClient(
+const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
   );
